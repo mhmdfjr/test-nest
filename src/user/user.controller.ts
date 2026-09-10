7973a085570b050c56129c8b141c844de2782b9e
mhmdfjr <moh.fajar1304@gmail.com>
2026-09-10 23:39:33 +0700

    feat: add controller user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Put } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { UpdateUserDto } from './dto/update-user.dto.js';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get() // GET /user
+  getUser(@Query('name') name: string) {
+    const users = [
+      { id: '1', name: 'John', adress: 'USA' },
+      { id: '2', name: 'Maria', adress: 'USA' },
+      { id: '3', name: 'Smith', adress: 'USA' },
+    ];
+
+    if (name) {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    }
+
+    return users;
+  }
+
+  @Get(':id') // GET /user/:id
+  getUserById(@Param('id') id: string) {
+    return { id: 1, name: 'Ken', address: 'USA' };
+  }
+
+  @Post() // POST /user
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return { data: createUserDto, message: 'User created successfully!' };
+  }
+
+  @Put(':id') // PUT /user/:id
+  updateUserById(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return {
+      data: { id, ...updateUserDto },
+      message: 'User updated successfully',
+    };
+  }
+}
